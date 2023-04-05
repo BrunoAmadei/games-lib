83ac528094534ba06b3f96f7e834a0fc7849287d
BrunoAmadei <brunoamadei98@gmail.com>
2023-04-05 13:35:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
