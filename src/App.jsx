1279a6ab7e5b4189bdb67bddfc14571fdd68943c
BrunoAmadei <brunoamadei98@gmail.com>
2023-04-06 14:10:04 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import { useState, useEffect } from 'react'
-import config from './config.jsx'
-
+import { useState, useEffect } from 'react';
+import config from './config';
 
 function App() {
-  const [gamesList, setGamesList] = useState([])
+  const [gamesList, setGamesList] = useState([]);
 
   useEffect(() => {
-    const loadAll = async () => {
-      const games = await config.getGamesList()
-      setGamesList(games[0].items.results)
-    }
-    loadAll()
-  }, [])
+    const loadGames = async () => {
+      const games = await config.getGamesList();
+      setGamesList(games[0].items.results);
+      console.log(games)
+    };
+    
+    loadGames();
+  }, []);
 
   return (
     <div className="App">
-      <h1>Lista de jogos:</h1>
-        
+      <h1>Games List:</h1>
+      <ul>
+        {gamesList.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
