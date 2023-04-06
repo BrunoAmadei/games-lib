612992ec1a70372463e844c397e022dba6d465f8
BrunoAmadei <brunoamadei98@gmail.com>
2023-04-06 14:19:59 -0300

    css update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import { useState, useEffect } from 'react';
-import config from './config';
+import React, { useState, useEffect } from "react";
+import config from "./config";
 
 function App() {
-  const [gamesList, setGamesList] = useState([]);
+  const [games, setGames] = useState([]);
 
   useEffect(() => {
     const loadGames = async () => {
       const games = await config.getGamesList();
-      setGamesList(games[0].items.results);
-      console.log(games)
+      setGames(games[0].items.results); // Acessando a lista de jogos
     };
-    
+
     loadGames();
   }, []);
 
   return (
-    <div className="App">
-      <h1>Games List:</h1>
-      <ul>
-        {gamesList.map((game) => (
-          <li key={game.id}>{game.name}</li>
-        ))}
-      </ul>
+    <div>
+      {games.map((game) => (
+        <div key={game.id}>
+          <h2>{game.name}</h2>
+          <img src={game.background_image} alt={game.name} width={300}/>
+        </div>
+      ))}
     </div>
   );
 }
