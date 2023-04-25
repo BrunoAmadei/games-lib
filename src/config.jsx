5b03b18855e9ec9d177583451e95742660fc99bc
BrunoAmadei <brunoamadei98@gmail.com>
2023-04-25 14:34:20 -0300

    Update config.jsx

--- src/config.jsx
+++ src/config.jsx
@@ -3,16 +3,19 @@ import axios from 'axios';
 const ApiKey = '8b04ba9a7cbc46ae93894d6a98ed2c29';
 const ApiUrl = 'https://api.rawg.io/api';
 
+const currentYear = new Date().getFullYear()
+const nextYear = currentYear + 1
+
 const fetchApi = async (endpoint) => {
-    try {
-      const response = await axios.get(`${ApiUrl}${endpoint}`);
-      const result = response.data
-      return result;
-    } catch (error) {
-      console.error(`Error: ${error.message}`);
-    }
-  };
-  
+  try {
+    const response = await axios.get(`${ApiUrl}${endpoint}`);
+    const result = response.data
+    return result;
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
+
 
 export default {
   getGamesList: async () => {
@@ -24,4 +27,13 @@ export default {
       },
     ];
   },
+  getToBeReleased: async () => {
+    return [
+      {
+        slug: 'beReleased',
+        title: 'ToBeReleased',
+        items: await fetchApi(`/games?key=${ApiKey}&dates=${currentYear}-01-01,${nextYear}-01-01&ordering=-added`)
+      }
+    ]
+  }
 };
